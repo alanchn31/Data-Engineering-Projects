ee0ea6890766d94c6c4b369f3384ee6416ed1eee
alanchn31 <alanchn31@gmail.com>
2020-05-17 11:55:26 +0800

    Add check for null Ids in quality check
    Check for nulls in song id and user id. If there are nulls, raise a
    ValueError

--- 6. Data Pipelining with Airflow/airflow/plugins/operators/data_quality.py
+++ 6. Data Pipelining with Airflow/airflow/plugins/operators/data_quality.py
@@ -19,6 +19,22 @@ class DataQualityOperator(BaseOperator):
     def execute(self, context):
         redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
         for table in self.table_names:
+            # Check that entries are being copied to table
             records = redshift.get_records(f"SELECT COUNT(*) FROM {table}")
             if len(records) < 1 or len(records[0]) < 1:
                 raise ValueError(f"Data quality check failed. {table} returned no results")
+
+        # Check that there are no rows with null ids
+        dq_checks=[
+            {'table': 'users',
+             'check_sql': "SELECT COUNT(*) FROM users WHERE userid is null",
+             'expected_result': 0},
+            {'table': 'songs',
+             'check_sql': "SELECT COUNT(*) FROM songs WHERE songid is null",
+             'expected_result': 0}
+        ]
+        for check in dq_checks:
+             records = redshift.get_records(check['check_sql'])
+             if records[0] != check['expected_result']:
+                raise ValueError(f"Data quality check failed. {check['table']} \
+                                   contains null in id column")
